045f3db61bea28c04bbb4c5585802e1d8dc26306
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2022-11-19 10:53:10 +0900

    update ver

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # nvc
+
+open nvim config with neovim
+
+# install
+
+```
+$ go install github.com/kaepa3/nvc@latest
+```
+
+# command
+
+add the below command
+
+```
+$nvc
+```
+
+# config
+
+can change command and args
+confing file is **~/.nvc.yaml**
+![config](conf.png "config")
+default command is nvim
+default args is "~/.config/nvim"
--- main.go
+++ main.go
@@ -2,18 +2,50 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
 )
 
+var (
+	confPath = "~/.nvc.yaml"
+)
+
+type Config struct {
+	Command string   `yaml:"command"`
+	Args    []string `yaml:"args"`
+}
+
 func main() {
-	if err := openEditor("nvim", "~/.config/nvim"); err != nil {
+	cmd, args, err := readConf()
+	if err != nil {
+		fmt.Println(err)
+	}
+	if err := openEditor(cmd, args...); err != nil {
 		fmt.Fprint(os.Stdout, fmt.Sprintf("failed open text editor. %s\n", err.Error()))
 		return
 	}
 }
-func openEditor(cmd string, arg string) error {
-	c := exec.Command(cmd, arg)
+
+func readConf() (string, []string, error) {
+	homePath := os.Getenv("HOME")
+	confPath := filepath.Join(homePath, "./.nvc.yaml")
+	buf, err := ioutil.ReadFile(confPath)
+	if err == nil {
+		data := Config{}
+		if err := yaml.Unmarshal(buf, &data); err == nil {
+			return data.Command, data.Args, nil
+		}
+	}
+	return "nvim", []string{"~/.config/nvim"}, err
+}
+
+func openEditor(cmd string, arg ...string) error {
+
+	c := exec.Command(cmd, arg...)
 	c.Stdin = os.Stdin
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
